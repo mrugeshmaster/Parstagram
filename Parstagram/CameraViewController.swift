4a2f48e34fe9805d0185084a9063193e1e79efe1
Mrugesh Master <mrugesh.master@gmail.com>
2020-10-03 14:16:12 -0700

    Parstagram Part 1

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -27,7 +27,10 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         post["author"] = PFUser.current()!
         
         let imageData  = imageView.image!.pngData()
-        let file = PFFileObject(name: "image.png", data:imageData!)
+        let file = PFFileObject(name : "image.png", data:imageData!)
+        
+        post["image"] = file
+        
         post.saveInBackground { (success, error) in
             if success {
                 print("Saved!")
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,18 +7,59 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var posts =  [PFObject]()
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.nameLabel.text = user.username
+        cell.commentLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
